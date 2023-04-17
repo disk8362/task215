bf677b74d219293937feaeb2ee5f49115412a386
disk8362 <disk8362@mail.ru>
2023-04-18 01:59:13 +0300

    git commit

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -1,9 +1,7 @@
 package koschei;
-
 import koschei.config.AppConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
